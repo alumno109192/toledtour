b2c877a6c726a3ccb0b5dfc8120a5d230290a6f7
alumno109192 <alumno109192@ies-azarquiel.es>
2025-05-18 15:15:29 +0200

    actualizado

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,42 +1,47 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.example.toledotour"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    compileSdk = 35  // Actualizado a SDK 35
+    ndkVersion = "27.0.12077973"  // Puedes eliminar esto si no usas NDK
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17  // Actualizado a Java 17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "17"  // Actualizado a Java 17
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.toledotour"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
+        minSdk = 21  // Definido explícitamente para V2 embedding
+        targetSdk = 35  // Actualizado a SDK 35
+        versionCode = flutter.versionCode.toInt()
         versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    // Puedes agregar aquí otras dependencias si las necesitas
 }
 
 flutter {
--- android/app/src/main/kotlin/com/example/toledotour/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/toledotour/MainActivity.kt
@@ -1,4 +1,4 @@
 // MainActivity.kt
-package your.package.name
+package com.example.toledotour
 import io.flutter.embedding.android.FlutterActivity
 class MainActivity : FlutterActivity()
